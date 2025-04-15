2b444fd74e02b88f944c8775f241f773b77ef7d7
schnell <christopher.schnell@hotmail.com>
2025-04-15 10:32:11 +0200

    setup for test

---
+++ backend/test/unit/test_introduction.py
@@ -0,0 +1,11 @@
+# 1. Make sure mandatory libraries are installed: pytest and pytest-cov (both included in requirements.pip)
+
+# 2. Setup the directory structure:
+# • create the (empty) file conftest.py at the root of the directory
+# • create test/unit/ subdirectory
+
+# 3. Create the first test file test_introduction.py
+# • Place it in the respective folder
+# • Important: file prefix must be “test_”
+
+# 4. Optionally: create a pytest.ini file, check it for more info
